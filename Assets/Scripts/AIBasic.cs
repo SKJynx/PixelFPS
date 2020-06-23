765359fd8d381e75e358d619d0eb8e3d8a7fc5fd
Nicholas Yee <SK_Jynx@hotmail.com>
2020-06-23 17:20:42 -0600

    Basic follow AI with LookAt script
    Script allows any sprite it's attached to, to look at the camera always. Similar to old DOOM games. Used basic follow AI to test it.

---
+++ Assets/Scripts/AIBasic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBasic : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float maxSpeed;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerCamera");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 moveToTarget = player.transform.position - transform.position;
+
+        rb.velocity = new Vector3(moveToTarget.x, 0, moveToTarget.z) * maxSpeed;
+
+    }
+}
